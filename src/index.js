c596d5237742f175c6700308c5ce879c8e509b45
Janderson P. Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2021-07-22 15:32:44 -0300

    feat: Finishing concepts module

--- src/index.js
+++ src/index.js
@@ -1,27 +1,55 @@
-const express = require('express');
+const express = require('express')
 
 const app = express()
 
+/**Middleware para usarmos JSON na nossa aplicação */
+app.use(express.json())
+
+/** GET - Buscar uma informação dentro do servidor
+ *  POST - Inserir uma informação no servidor
+ *  PUT - Alterar uma informação no servidor
+ *  PATCH - Alterar uma informação específica
+ *  DELETE - Deletar uma informação no servidor
+ */
+
+/** Tipos de parâmetros
+ *
+ * Route params - Identificar um recurso editar/deletar/buscar
+ * Query params - Paginação/Filtro
+ * Body params - Objetos para inserção/alteração de um recurso (JSON)
+ */
 app.get('/courses', (request, response) => {
-  return response.json(["Curso 1","Curso 2","Curso 3"])
-});
+  const query = request.query
+
+  console.log(query)
+
+  return response.json(['Curso 1', 'Curso 2', 'Curso 3'])
+})
 
 app.post('/courses', (request, response) => {
-  return response.json(["Curso 1","Curso 2","Curso 3", "Curso 4"])
-});
+  const body = request.body
+
+  console.log(body)
+
+  return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4'])
+})
 
 app.put('/courses/:id', (request, response) => {
-  return response.json(["Curso 6","Curso 2","Curso 3", "Curso 4"])
-});
+  const { id } = request.params
+
+  console.log(id)
+
+  return response.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4'])
+})
 
 app.patch('/courses/:id', (request, response) => {
-  return response.json(["Curso 6","Curso 7","Curso 3", "Curso 4"])
-});
+  return response.json(['Curso 6', 'Curso 7', 'Curso 3', 'Curso 4'])
+})
 
 app.delete('/courses/:id', (request, response) => {
-  return response.json(["Curso 6","Curso 7", "Curso 4"])
-});
+  return response.json(['Curso 6', 'Curso 7', 'Curso 4'])
+})
 
 app.listen(3333, () => {
-  console.log('✔ Server stated on port 3333');
+  console.log('✔ Server stated on port 3333')
 })
